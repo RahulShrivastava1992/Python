28dbe857f6ebee4983cee4a94e759626b86f2cd1
Rahul shrivastava <rahulsrivastava.12abes@hotmail.com>
2018-12-29 19:43:00 +0530

    rendering in web page

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import re
+from datetime import datetime
 
 def home(request):
     return HttpResponse("Hello, Django! Rahul is here now to take care")
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
 # Create your views here.
